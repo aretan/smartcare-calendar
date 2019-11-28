3550c83bed516bd4e0a21af9d6cc017784ba2d69
aretan <aretan@outlook.jp>
2019-11-28 14:10:41 +0900

    fix

--- application/app/Views/Calendar/Show.php
+++ application/app/Views/Calendar/Show.php
@@ -572,10 +572,11 @@
 
               title = info.description;
               if (!title) {
-                  parent = eventData.find(function(events){
-                      return events.color == info.source.color
+                  eventData.forEach(function(events){
+                      if (events.color == info.source.color) {
+                          title = events.description;
+                      }
                   });
-                  title = parent.description;
               }
 
               $(element).prop('title', title);
@@ -795,30 +796,6 @@
   function month(target) {
       $("html,body").animate({scrollTop:$('#calendar').offset().top});
       $('#calendar').fullCalendar('gotoDate', target);
-  }
-
-  // find polifill for IE11
-  if (!Array.prototype.find) {
-      Array.prototype.find = function(predicate) {
-          if (this === null) {
-              throw new TypeError('Array.prototype.find called on null or undefined');
-          }
-          if (typeof predicate !== 'function') {
-              throw new TypeError('predicate must be a function');
-          }
-          var list = Object(this);
-          var length = list.length >>> 0;
-          var thisArg = arguments[1];
-          var value;
-
-          for (var i = 0; i < length; i++) {
-              value = list[i];
-              if (predicate.call(thisArg, value, i, list)) {
-                  return value;
-              }
-          }
-          return undefined;
-      };
   }
 </script>
 <?= $this->endSection() ?>
