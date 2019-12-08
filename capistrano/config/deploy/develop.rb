0b8be461e9a0b63573e4dbc78f26acfc6d3f6c9e
aretan <aretan@outlook.jp>
2019-12-08 15:58:59 +0900

    domai

--- capistrano/config/deploy/develop.rb
+++ capistrano/config/deploy/develop.rb
@@ -6,7 +6,7 @@
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
-server "18.177.141.101", user: "ubuntu", roles: %w{web}
+server "smartcare-calendar.net", user: "ubuntu", roles: %w{web}
 
 # role-based syntax
 # ==================
