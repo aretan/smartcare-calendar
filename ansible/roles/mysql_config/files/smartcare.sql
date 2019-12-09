8e6c5d3eca17515c2f7ce18d98d02065a15c0d66
aretan <aretan@outlook.jp>
2019-12-09 17:45:44 +0900

    remove testdata & fix length

--- ansible/roles/mysql_config/files/smartcare.sql
+++ ansible/roles/mysql_config/files/smartcare.sql
@@ -3,7 +3,7 @@ INSTALL SONAME 'server_audit';
 START TRANSACTION;
 
 CREATE TABLE `shoken` (
-  `id` char(10) NOT NULL PRIMARY KEY COMMENT '証券番号',
+  `id` char(9) NOT NULL PRIMARY KEY COMMENT '証券番号',
   `name` varchar(255) NOT NULL COMMENT '被保険者名',
   `date` date NOT NULL COMMENT '契約開始日',
   `comment` text DEFAULT NULL COMMENT '査定者コメント',
@@ -13,8 +13,8 @@ CREATE TABLE `shoken` (
 ) COMMENT='証券';
 
 CREATE TABLE `ukeban` (
-  `id` char(16) NOT NULL PRIMARY KEY COMMENT '受付番号',
-  `shoken_id` char(10) NOT NULL COMMENT '証券番号',
+  `id` char(14) NOT NULL PRIMARY KEY COMMENT '受付番号',
+  `shoken_id` char(9) NOT NULL COMMENT '証券番号',
   `date` date NOT NULL COMMENT '受付日付',
   `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -25,8 +25,8 @@ CREATE TABLE `ukeban` (
 
 CREATE TABLE `shujutsu` (
   `id` int(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY NOT NULL COMMENT '手術ID (自動採番)',
-  `shoken_id` char(10) NOT NULL COMMENT '証券番号',
-  `ukeban_id` char(16) NOT NULL COMMENT '受付番号',
+  `shoken_id` char(9) NOT NULL COMMENT '証券番号',
+  `ukeban_id` char(14) NOT NULL COMMENT '受付番号',
   `date` date NOT NULL COMMENT '手術日',
   `warrantyStart` date NOT NULL COMMENT '保証開始日',
   `warrantyEnd` date NOT NULL COMMENT '保証終了日',
@@ -40,8 +40,8 @@ CREATE TABLE `shujutsu` (
 
 CREATE TABLE `nyuin` (
   `id` int(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY NOT NULL COMMENT '入院ID (自動採番)',
-  `shoken_id` char(10) NOT NULL COMMENT '証券番号',
-  `ukeban_id` char(16) NOT NULL COMMENT '受付番号',
+  `shoken_id` char(9) NOT NULL COMMENT '証券番号',
+  `ukeban_id` char(14) NOT NULL COMMENT '受付番号',
   `start` date NOT NULL COMMENT '入院開始日',
   `end` date NOT NULL COMMENT '入院終了日',
   `warrantyStart` date NOT NULL COMMENT '保証開始日',
@@ -56,8 +56,8 @@ CREATE TABLE `nyuin` (
 
 CREATE TABLE `tsuin` (
   `id` int(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY NOT NULL COMMENT '通院ID (自動採番)',
-  `shoken_id` char(10) NOT NULL COMMENT '証券番号',
-  `ukeban_id` char(16) NOT NULL COMMENT '受付番号',
+  `shoken_id` char(9) NOT NULL COMMENT '証券番号',
+  `ukeban_id` char(14) NOT NULL COMMENT '受付番号',
   `date` date NOT NULL COMMENT '通院日',
   `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -68,8 +68,8 @@ CREATE TABLE `tsuin` (
 
 CREATE TABLE `bunsho` (
   `id` int(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY NOT NULL COMMENT '通院ID (自動採番)',
-  `shoken_id` char(10) NOT NULL COMMENT '証券番号',
-  `ukeban_id` char(16) NOT NULL COMMENT '受付番号',
+  `shoken_id` char(9) NOT NULL COMMENT '証券番号',
+  `ukeban_id` char(14) NOT NULL COMMENT '受付番号',
   `date` date NOT NULL COMMENT '文書日',
   `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
