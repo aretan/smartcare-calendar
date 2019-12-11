0caf6a7d4c6a0e75aeae72171624b606ec2cb6d9
aretan <aretan@outlook.jp>
2019-12-11 20:51:09 +0900

    stub hangarian

---
+++ travis/stub.php
@@ -0,0 +1,10 @@
+<?php
+
+require "../application/vendor/autoload.php";
+
+$matrix = json_decode(file_get_contents('php://input'), true);
+$allocation = $matrix ? (new \Hungarian\Hungarian($matrix))->solve() : [];
+foreach ($allocation as $i => $v) {
+    $result[] = [$i, $v];
+}
+echo json_encode($result);
